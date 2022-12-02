7072b6662c06d18cf0a61a24ec33e44ed74ed945
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-02 20:36:50 +0900

    rayout system

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import reactDom from "react-dom";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text
-        style={{
-          color: "red",
-          fontSize: 30,
-        }}
-      >
-        Hello, I'm Wonjang
-      </Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "red" }}></View>
+      <View style={{ flex: 3, backgroundColor: "blue" }}></View>
+      <View style={{ flex: 1, backgroundColor: "green" }}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  stretch: {
+    width: 200,
+    height: 200,
+    resizeMode: "stretch",
+  },
+});
