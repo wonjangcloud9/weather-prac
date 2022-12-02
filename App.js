5f6a97761e8b148d145f23a33d2263cb7e20cbc0
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-02 20:19:54 +0900

    rules of native

--- App.js
+++ App.js
@@ -3,18 +3,25 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, I'm Wonjang</Text>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text
+        style={{
+          color: "red",
+          fontSize: 30,
+        }}
+      >
+        Hello, I'm Wonjang
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
