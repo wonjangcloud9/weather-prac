35010053f7941aee13c5046d42f522b2d256c878
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-12-02 20:45:17 +0900

    styles

--- App.js
+++ App.js
@@ -4,18 +4,49 @@ import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "red" }}></View>
-      <View style={{ flex: 3, backgroundColor: "blue" }}></View>
-      <View style={{ flex: 1, backgroundColor: "green" }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  stretch: {
-    width: 200,
-    height: 200,
-    resizeMode: "stretch",
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+  },
+  weather: {
+    flex: 3,
+    backgroundColor: "teal",
+  },
+  day: {
+    flex: 1,
+    backgroundColor: "teal",
+
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 48,
   },
 });
